9c7430542fe2583dba5930a7ebcc33a4f60a437f
lpatiny <gb252243>
2016-10-22 23:38:02 +0200

    Improve formatting

--- searchAndFormat.js
+++ searchAndFormat.js
@@ -19,20 +19,19 @@ module.exports = function searchAndFormat(query) {
         };
     }
 
-
     
     var result = '';
     result += `<b>${element.symbol}</b> : <em>${element.name}</em>\r\n`;
-    result += `<b>Element number: </b>${element.Z}\r\n`;
-    result += `<b>French name: </b>${element.nameFR}\r\n`;
+    result += `<b>Z: </b>${element.Z}\r\n`;
+    result += `<b>In french: </b>${element.nameFR}\r\n`;
     result += `<b>Atomic weight: </b>${element.atomicWeight} ${info.atomicWeight.unit}\r\n`;
-    result += `<b>Melting point: </b>${element.melting} ${info.melting.unit}`+' ('+(element.melting-273.15).toPrecision(4)+' °C)\r\n';
-    result += `<b>Boiling point: </b>${element.boiling} ${info.boiling.unit}`+' ('+(element.boiling-273.15).toPrecision(4)+' °C)\r\n';
+    result += `<b>m.p.: </b>${element.melting} ${info.melting.unit}`+' ('+(element.melting-273.15).toPrecision(4)+' °C)\r\n';
+    result += `<b>b.p.: </b>${element.boiling} ${info.boiling.unit}`+' ('+(element.boiling-273.15).toPrecision(4)+' °C)\r\n';
     result += `<b>Electronegativity: </b>${element.electronegativity}\r\n`;
-    result += `<b>First ionisation energy: </b>${element.firstIonisation} ${info.firstIonisation.unit}\r\n`;
-    result += `<b>Electronic configuration: </b>${element.electronConfiguration}\r\n`;
-    result += `<b>First ionisation energy: </b>${element.firstIonisation} ${info.firstIonisation.unit}\r\n`;
-    result += getIsotopes(element.symbol)+'\r\n';
+    result += `<b>Ionisation energy: </b>${element.firstIonisation} ${info.firstIonisation.unit}\r\n`;
+    result += `<b>Configuration: </b>${element.electronConfiguration}\r\n`;
+    result += getIsotopes(element.symbol);
+    
     return {
         string: result,
         name: element.name
@@ -46,9 +45,12 @@ function getIsotopes(symbol) {
             if (! element.isotopes || element.isotopes.length===0) {
                 return '<b>No stable isotopes</b>';
             } else {
-                return '<b>Stable isotopes: </b>'+element.isotopes.map(
-                        i => Math.round(i.mass)+symbol+" ("+i.percentage.toFixed(2)+"%)"
-                    ).join(", ");
+                return '<b>Stable isotopes: </b>\r\n'+
+                        '<pre>'+
+                        element.isotopes.map(
+                                i => '  '+Math.round(i.mass)+symbol+" - "+i.mass.toFixed(5)+" - "+i.percentage.toFixed(2)+"%"
+                        ).join("\r\n")+
+                        '</pre>';
             }
         }
     }
